d10c3b8eeb3b189b6dcb2911767a5294950894e1
Nguyễn Hữu Nghĩa <94152859+huunghia040603@users.noreply.github.com>
2025-07-21 14:43:52 +0700

    up

---
+++ index.py
@@ -0,0 +1,39 @@
+from flask import Flask, app
+from flask import render_template, request, redirect, jsonify, session, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/dai-hoc-hcm")
+def dai_hoc_hcm():
+    return render_template('dh-hcm.html')
+
+@app.route("/truongdaihoc/ou")
+def dai_hoc_ou():
+    return render_template('truongdaihoc/daihocmo.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route("/dangnhap")
+def dangnhap():
+    return render_template('dangnhap.html')
+
+@app.route("/dangky")
+def dangky():
+    return render_template('dangnhap.html')
+
+@app.route("/mbti")
+def mbti():
+    return render_template('/khampha/mbti.html')
+
+@app.route("/mbti16")
+def mbti16():
+    return render_template('/khampha/mbti16.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
